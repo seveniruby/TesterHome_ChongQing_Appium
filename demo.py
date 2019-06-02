8abe77b5e8dfa1bdf4963653ef878bd488db3482
seveniruby <seveniruby@gmail.com>
2019-06-02 17:05:08 +0800

    add xpath demo

--- demo.py
+++ demo.py
@@ -39,6 +39,10 @@ class TestXueqiu(unittest.TestCase):
         self.driver.find_element_by_xpath("//*[@text='BABA']").click()
         assert float(self.driver.find_element_by_id("stock_current_price").text)<200
 
+    def test_mi(self):
+        self.driver.find_element_by_id("tv_search").click()
+        self.driver.find_element_by_id("search_input_text").send_keys("xiaomi")
+        self.driver.find_element_by_xpath("//*[@text='01810']/../../..//*[contains(@resource-id, '_btn')]").click()
 
     def tearDown(self) -> None:
         self.driver.quit()
